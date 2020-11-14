50fd5b6a24bfb999d20850c9456e336180c1aa9c
unknown <metybuza46@gmail.com>
2020-11-14 13:47:03 -0700

    Updates

---
+++ precache-manifest.78116e54a2b13a140e326e4828dfb5bd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5bc0aca513e81712d638cfb8f16f00",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "06bd91b1948ce97c6ac4",
+    "url": "/react-portfolio/static/css/main.5aff2c5b.chunk.css"
+  },
+  {
+    "revision": "d9d5af2ada536026219b",
+    "url": "/react-portfolio/static/js/2.fb8d727b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.fb8d727b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06bd91b1948ce97c6ac4",
+    "url": "/react-portfolio/static/js/main.74b8152c.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  },
+  {
+    "revision": "3f1774650b881ea9f352ba17d082882a",
+    "url": "/react-portfolio/static/media/BiteReport.3f177465.png"
+  },
+  {
+    "revision": "15ad861d59982c6d45bcd673643b1af7",
+    "url": "/react-portfolio/static/media/background.15ad861d.jpg"
+  },
+  {
+    "revision": "86a9afc6ede779e92d7c020b4acb2cf4",
+    "url": "/react-portfolio/static/media/burgerapp.86a9afc6.png"
+  },
+  {
+    "revision": "896f9a83236bd9eea3bd9426b8daf9f4",
+    "url": "/react-portfolio/static/media/citysidekick.896f9a83.png"
+  },
+  {
+    "revision": "65614eb9088c05a34e8dff7d2f3ac2de",
+    "url": "/react-portfolio/static/media/codequiz.65614eb9.jpg"
+  },
+  {
+    "revision": "c4d4583e328d5a72a4878eab33b07988",
+    "url": "/react-portfolio/static/media/html.c4d4583e.jpg"
+  },
+  {
+    "revision": "b591dcb3477b3001be04172305e91960",
+    "url": "/react-portfolio/static/media/js.b591dcb3.png"
+  },
+  {
+    "revision": "2e4b86842609ed971761519a2b558707",
+    "url": "/react-portfolio/static/media/mongo.2e4b8684.png"
+  },
+  {
+    "revision": "a17807c14ae87e39320c6979705146f8",
+    "url": "/react-portfolio/static/media/mypic.a17807c1.jpg"
+  },
+  {
+    "revision": "51fcab8705ee70a51035126df56798f3",
+    "url": "/react-portfolio/static/media/mysql.51fcab87.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/react-portfolio/static/media/nodejslogo.6dcf6999.png"
+  },
+  {
+    "revision": "efad41ff9460b61a4076cb7f79250940",
+    "url": "/react-portfolio/static/media/notetaker.efad41ff.png"
+  },
+  {
+    "revision": "984214c7d0320e202034085d5dafa460",
+    "url": "/react-portfolio/static/media/resume-icon.984214c7.PNG"
+  },
+  {
+    "revision": "0c7378626898abb51bcad538779c1b45",
+    "url": "/react-portfolio/static/media/resume.0c737862.pdf"
+  },
+  {
+    "revision": "dcbfe8b65ad59234192e73c628350bf9",
+    "url": "/react-portfolio/static/media/weatherdash.dcbfe8b6.png"
+  }
+]);
