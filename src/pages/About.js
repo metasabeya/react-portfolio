ceed800468275398be4309d66d77d1090dc9025b
unknown <metybuza46@gmail.com>
2020-11-13 18:19:52 -0700

    updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,7 +18,13 @@ function About() {
                   alt="my pic" />
                 <div className="bio">
                   <p>
-                  Hello everyone, My name is Metasbeya Ketsela.I am orignally from Ethiopia.i just came to Usa in 2019.When i was in Ethiopia i have completed two degrees which is Bachlor in Accounting and finance and the other one is Masters in Business Administration. when i was their have been working in one of the best known bank in Ethiopia which is Bank of Abyssinia.I am married but no childrens.currently i am astudent of university of denver boot camp in web development ,its completly different with my preivious experiance but i loved it.In my free time I enjoy reading and i love practicing to code.In short this is all about me ,if you need more information please visit my contact information icon.Thank you for visting my portfolio website.</p>
+                  Hello everyone, My name is Metasbeya Ketsela.I am orignally from Ethiopia.i just came to Usa in 2019.When i was in Ethiopia i have completed two degrees which is Bachlor in Accounting and finance and the other one is Masters in Business Administration. when i was their have been working in one of the best known bank in Ethiopia which is Bank of Abyssinia.I am married but no childrens.</p>
+                  <p>
+                  Am on the way to complete the program university of denver boot camp in web development(coding) ,its completly different with my preivious experiance but i loved it.In my free time I enjoy reading and i love practicing to code.
+                  </p>
+                  <p>
+                  Now I'm a full stack developer and am open for any junior postion.In short this is all about me Thank you for visting my portfolio website.
+                  </p>
                   </div>
               </Col>
             </Row>
