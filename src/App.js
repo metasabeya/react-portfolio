e4b1de467bb486f657e0c7d5ea42591895f6596b
unknown <metybuza46@gmail.com>
2020-11-13 18:33:46 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Router>
         <Container>
           <Row>
-            <h1 className="rounded col-sm-12 col-lg-3">Jen Chen</h1>
+            <h1 className="rounded col-sm-12 col-lg-3">Metasabeya</h1>
             <Navbar/>
             {/* use switch if you want to add "*" path */}
             <Switch>
