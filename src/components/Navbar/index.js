c27ae8aa61f5419a75b2f1fc68310db08ad40f32
unknown <metybuza46@gmail.com>
2020-11-14 20:48:57 -0700

    final commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
     <nav className="nav nav-pills flex-column flex-sm-row col-sm-12 col-lg-9">
@@ -47,9 +46,7 @@ function Navbar() {
         <a
           className="flex-sm-fill text-sm-center nav-link resume"
           href={require("../assets/images/resume.pdf")}
-        //   onClick={() => window.open("../assets/images/jane-doe-resume.pdf")}
-        //   href={Resume}
-        // onClick={() => window.open(Resume)}
+        
           target="_blank"
           rel="noopener noreferrer"
         >
